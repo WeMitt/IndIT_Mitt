8c59e895b9cde57e39b16baee04e2312051ee0b8
WeMitt <132549215+WeMitt@users.noreply.github.com>
2023-05-04 11:39:41 +0200

    Add files via upload

--- Lab3_04052023_Mittmannsgruber.py
+++ Lab3_04052023_Mittmannsgruber.py
@@ -1,14 +1,14 @@
 int_a=int(input("Eingabe Zahl1:"))
-if int_a == 0:
-    while int_a==0:
-        print("Die Zahl ist Null!")
-        int_a=int(input("Gib die Zahl erneut ein:"))
+#if int_a == 0:
+while int_a==0:
+    print("Die Zahl ist Null!")
+    int_a=int(input("Gib die Zahl erneut ein:"))
    
 int_b=int(input("Eingabe Zahl2:"))
-if int_b == 0:
-    while int_b==0:   
-        print("Die Zahl ist Null!")
-        int_b=int(input("Gib die Zahl erneut ein:"))
+#if int_b == 0:
+while int_b==0:   
+    print("Die Zahl ist Null!")
+    int_b=int(input("Gib die Zahl erneut ein:"))
 
 print(int_a+int_b)
 print(abs(int_a-int_b))
