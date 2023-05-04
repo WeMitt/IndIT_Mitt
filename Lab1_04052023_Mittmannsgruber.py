9690283dff4e07c557ba1139583911b9eb2ad38b
WeMitt <132549215+WeMitt@users.noreply.github.com>
2023-05-04 10:30:15 +0200

    Add files via upload

---
+++ Lab1_04052023_Mittmannsgruber.py
@@ -0,0 +1,10 @@
+int_a=int(input("Eingabe Zahl1:"))
+int_b=int(input("Eingabe Zahl2:"))
+
+print(int_a+int_b)
+print(abs(int_a-int_b))
+print(int_a*int_b)
+print(int_a/int_b, "oder", int_b/int_a)
+    
+
+    
